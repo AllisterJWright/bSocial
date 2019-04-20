24904bf60c578eca2d28a88400ce6059dacbd399
cassion <allister.wright@gmail.com>
2019-04-20 22:40:48 +0000

    Removed prefix and suffix properties from dispatcher-servlet, reconfigured UserController to test

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.app.model.User;
-import com.app.service.S3Service;
 import com.app.service.UserService;
 
 @Controller
@@ -21,13 +21,11 @@ public class UserController
 	// @CrossOrigin is used to handle the request from a different origin
 	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(value="/register", method=RequestMethod.POST)
-	public @ResponseBody String registerUser(@PathVariable(name = "name") String displayName, 
-											@PathVariable(name = "email") String email, 
-											@PathVariable(name = "username") String username, 
-											@PathVariable(name = "password") String password)
+	public @ResponseBody String registerUser(@RequestBody String name, String email, String username, String password)
 	{
+//		System.out.println(jsonString);
 		String defaultProfileImage = "default-profile-image";
-		User user = new User(username, password, email, displayName, defaultProfileImage);
+		User user = new User(username, password, email, name, defaultProfileImage);
 		userService.insertUser(user);
 		return "login";
 	}
